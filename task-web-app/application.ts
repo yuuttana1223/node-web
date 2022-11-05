cedf6096cc967cec8584bb312b48958c779659a5
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2022-11-05 14:54:26 +0900

    ✨ アプリケーション・サーバーを追加

---
+++ task-web-app/application.ts
@@ -0,0 +1,70 @@
+import { createServer } from "http";
+import { StatusCodes } from "http-status-codes";
+
+const SERVER = {
+  HOST: "127.0.0.1",
+  PORT: 8080,
+} as const;
+
+const tasks = [
+  {
+    title: "フロントエンドの実装",
+    createdAt: new Date(),
+  },
+  {
+    title: "サーバサイドの実装",
+    createdAt: new Date(),
+  },
+];
+
+const getTasksHTML = () => {
+  const tasksHTML = tasks
+    .map((task) => {
+      return `
+      <tr>
+      <td>${task.title}</td>
+      <td>${task.createdAt}</td>
+    </tr>`;
+    })
+    .join("");
+
+  return `<!DOCTYPE html>
+  <html lang="ja">
+  <head>
+    <meta charset="utf-8" />
+    <title>tasks</title>
+  </head>
+  <body>
+    <h1>タスク一覧</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>タイトル</th>
+          <th>作成日時</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${tasksHTML}
+      </tbody>
+    </table>
+  </body>
+</html>
+    `;
+};
+
+createServer((request, response) => {
+  // httpを使っていないときはごちゃごちゃやって取り出していた
+  const path = request.url;
+  if (path === "/tasks") {
+    response.writeHead(StatusCodes.OK);
+    const responseBody = getTasksHTML();
+    response.write(responseBody);
+    response.end();
+    return;
+  }
+
+  response.writeHead(StatusCodes.NOT_FOUND);
+  response.end();
+}).listen(SERVER.PORT, SERVER.HOST, () => {
+  console.log(`Server running at http://${SERVER.HOST}:${SERVER.PORT}/`);
+});
